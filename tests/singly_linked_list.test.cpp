cc9b38d41fddf3a070252dad54d5f9e7d6f04fba
@damianWu <damian.wolonkiewicz@gmail.com>
2022-06-19 23:50:52 +0200

    Added tests

--- tests/singly_linked_list.test.cpp
+++ tests/singly_linked_list.test.cpp
@@ -2,33 +2,194 @@
 
 #include <gtest/gtest.h>
 
-// #include <linked_lists/linked_lists.hpp>
+#include <linked_lists/linked_lists.hpp>
 
 namespace {
+
+using singly_linked_list::Element;
+using singly_linked_list::SinglyLinkedList;
 
 TEST(SinglyLinkedListPushBackTest, PushBackTest1) {
     // given
     int expected{-2005};
-    // singly_linked_list::SinglyLinkedList sll;
+    SinglyLinkedList sll;
 
     // when
-    // sll.push_back(-2005);
-    // int result{sll.cfront()->value};
+    sll.push_back(-2005);
+    int result{sll.cfront()->value};
 
-    // // then
-    ASSERT_EQ(-2005, expected);
+    // then
+    ASSERT_EQ(result, expected);
 }
 
-// Find tests
-// TEST(SinglyLinkedListFindTest, FindTest1) {
-//     // given
-//     singly_linked_list::SinglyLinkedList sll;
-//     sll.push_back(2);
-//     sll.push_back(5);
-//     sll.push_back(10);
-//     sll.push_back(15);
-//     // when
-//     // then
-// }
+TEST(SinglyLinkedListPushBackTest, PushBackTest2) {
+    // given
+    int expected{-2005};
+    SinglyLinkedList sll;
+
+    // when
+    sll.push_back(-2005);
+    int result{sll.cback()->value};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListPushBackTest, PushBackTest3) {
+    // given
+    int expected{-2005};
+    SinglyLinkedList sll;
+
+    // when
+    sll.push_back(-2005);
+    int result{sll.cfront()->value};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListNullElementTest, PushBackTestFrontIsNull) {
+    // given
+    const Element* expected{nullptr};
+    SinglyLinkedList sll;
+
+    // when
+    const auto* result{sll.cfront()};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListNullElementTest, PushBackTestBackIsNull) {
+    // given
+    const Element* expected{nullptr};
+    SinglyLinkedList sll;
+
+    // when
+    const auto* result{sll.cback()};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListIsEmptyTest, IsEmptyTest) {
+    // given
+    SinglyLinkedList sll;
+    bool expected{true};
+
+    // when
+    auto result{sll.empty()};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListIsNotEmptyTest, IsNotEmptyTest) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(2);
+    sll.push_back(-756);
+
+    bool expected{false};
+
+    // when
+    auto result{sll.empty()};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListElementAccessTest, ElementAccessTest1) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(-2);
+    sll.push_back(12);
+
+    int expected{10};
+
+    // when
+    auto result_a{sll.find(-2)->value};
+    auto result_b{sll.find(12)->value};
+    auto result{result_a + result_b};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListElementAccessTest, ElementAccessTest2) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(-2);
+    sll.push_back(12);
+    sll.push_back(1);
+    sll.push_back(10);
+
+    int expected{21};
+
+    // when
+    auto result_a{sll.find(-2)->value};
+    auto result_b{sll.find(12)->value};
+    auto result_c{sll.find(1)->value};
+    auto result_d{sll.find(10)->value};
+    auto result{result_a + result_b + result_c + result_d};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListElementAccessTest, ElementAccessTest3) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(1);
+    sll.push_back(-1);
+
+    int expected{0};
+
+    // when
+    auto result_a_ptr{sll.find(-1)};
+    auto result_b_ptr{sll.find(1)};
+    auto result_a{sll.find(result_a_ptr)};
+    auto result_b{sll.find(result_b_ptr)};
+    auto result{result_a->value + result_b->value};
+
+    // then
+    ASSERT_EQ(result, expected);
+}
+
+TEST(SinglyLinkedListElementAccessTest, ElementAccessTest4) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(8734);
+    sll.push_back(711);
+
+    int expected1{8734};
+    int expected2{711};
+
+    // when
+    auto result_a{sll.find(8734)->value};
+    auto result_b{sll.find(711)->value};
+    auto result{expected1 == result_a && expected2 == result_b};
+
+    // then
+    ASSERT_TRUE(result);
+}
+
+TEST(SinglyLinkedListElementAccessTest, ElementAccessTest5) {
+    // given
+    SinglyLinkedList sll;
+    sll.push_back(8734);
+    sll.push_back(711);
+
+    int expected1{8734};
+    int expected2{711};
+
+    // when
+    auto result_a{sll.find(8734)->value};
+    auto result_b{sll.find(711)->value};
+    auto result{expected1 != result_a && expected2 != result_b};
+
+    // then
+    ASSERT_FALSE(result);
+}
 
 }  // namespace
