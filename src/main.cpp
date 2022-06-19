b404ad18b171ec808f4e03a4b23516f1926f863a
@damianWu <damian.wolonkiewicz@gmail.com>
2022-06-19 23:49:56 +0200

    Modify main() function code

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,21 @@
 
 #include <iostream>
 
+#include "linked_lists/linked_lists.hpp"
+
 int main() {
     try {
+        singly_linked_list::SinglyLinkedList sll;
+        sll.push_back(-2005);
+
+        // int result{sll.cfront()->value};
+        // std::cout << result << '\n';
     } catch (const std::exception &e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
     } catch (...) {
         std::cerr << "Unknown type of exception catch in main function" << '\n';
     }
-    std::cout << "======================main() end reached";
+    std::cout << "======================main() end reached\n";
     return 0;
 }
