0cadcc540b0a5434eba83512c9ff1c46689c1471
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2022-06-19 19:37:05 +0200

    Initial implementation and added tests

--- src/linked_lists/linked_lists.cpp
+++ src/linked_lists/linked_lists.cpp
@@ -2,6 +2,38 @@
 
 #include "linked_lists/linked_lists.hpp"
 
-namespace singly_linked_list {}  // namespace singly_linked_list
+namespace singly_linked_list {
+
+const Element *SinglyLinkedList::cfront() const { return first_; }
+const Element *SinglyLinkedList::cback() const { return last_; }
+
+Element *SinglyLinkedList::find(const int x) const {
+    Element *current{first_};
+    while (current != nullptr) {
+        if (current->value == x) {
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
+}
+
+void SinglyLinkedList::push_back(const int x) {
+    auto *new_element = new Element{x};
+
+    if (!first_) {
+        first_ = new_element;
+        last_ = new_element;
+    } else {
+        last_->next = new_element;
+        last_ = new_element;
+    }
+}
+
+// void SinglyLinkedList::remove(int x);
+
+// void SinglyLinkedList::push_front(int x);
+
+}  // namespace singly_linked_list
 
 namespace doubly_linked_list {}  // namespace doubly_linked_list
--- src/linked_lists/linked_lists/linked_lists.hpp
+++ src/linked_lists/linked_lists/linked_lists.hpp
@@ -3,7 +3,48 @@
 #ifndef SRC_LINKED_LISTS_LINKED_LISTS_LINKED_LISTS_HPP_
 #define SRC_LINKED_LISTS_LINKED_LISTS_LINKED_LISTS_HPP_
 
-namespace singly_linked_list {}  // namespace singly_linked_list
+namespace singly_linked_list {
+
+enum class SEARCH_RESULT { FAIL, SUCCESS };
+
+struct Element {
+    int value{};
+    Element *next{nullptr};  // pointer to the next element
+};
+
+class SinglyLinkedList {
+    friend SinglyLinkedList operator+(const SinglyLinkedList &,
+                                      const SinglyLinkedList &);
+
+ public:
+    const Element *cfront() const;
+    const Element *cback() const;
+    Element *find(int) const;
+
+    void remove(int);
+    void push_back(int);
+    void push_front(int);
+
+    ~SinglyLinkedList();
+
+ private:
+    Element *first_;
+    Element *last_;
+};
+
+// TODO(@damianWu) Is this correct?
+inline SinglyLinkedList::~SinglyLinkedList() {
+    Element *current{first_};
+    while (current != nullptr) {
+        Element *next{current->next};
+        delete current;
+        current = next;
+    }
+    first_ = nullptr;
+    last_ = nullptr;
+}
+
+}  // namespace singly_linked_list
 
 namespace doubly_linked_list {}  // namespace doubly_linked_list
 
