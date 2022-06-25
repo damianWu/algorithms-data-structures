601ca76fc15a81635b625fcedd6d330e28120669
@damianWu <damian.wolonkiewicz@gmail.com>
2022-06-25 23:20:53 +0200

    main() changed

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,23 @@
 #include "linked_lists/linked_lists.hpp"
 
 int main() {
+    using singly_linked_list::SinglyLinkedList;
     try {
-        singly_linked_list::SinglyLinkedList sll;
-        sll.push_back(-2005);
+        // given
+        constexpr int before{12};
+        constexpr int new_value{542};
+        constexpr int after{13};
+        constexpr int pos{2};
+        SinglyLinkedList sll{before, after};
 
-        // int result{sll.cfront()->value};
-        // std::cout << result << '\n';
-    } catch (const std::exception &e) {
+        // when
+        sll.insert(pos, new_value);
+        auto* before_result{sll.find(before)};
+        auto* result{sll.find(new_value)};
+        // auto* after_result{sll.find(after)};
+        std::cout << before_result->next << '\n';
+        std::cout << result << '\n';
+    } catch (const std::exception& e) {
         std::cerr << "Exception catch in main function with message: "
                   << e.what() << '\n';
     } catch (...) {
