080e6e0bedb2b8d037d80b417ba38b4041df77a2
@damianWu <damian.wolonkiewicz@gmail.com>
2022-06-25 23:16:49 +0200

    Added push_front(), pop_back(), pop_front(), find_position(), insert()

--- src/linked_lists/linked_lists.cpp
+++ src/linked_lists/linked_lists.cpp
@@ -65,19 +65,88 @@ void SinglyLinkedList::push_front(const int x) {
     push_front(new_element);
 }
 
-    // If linked list is empty
-    if (!static_cast<bool>(first_)) {
-        first_ = new_element;
-        last_ = new_element;
-        return;
+void SinglyLinkedList::push_front(Element *const new_element) {
+    if (new_element) {
+        // if first_ contains sth
+        if (empty()) {
+            // if first_ is null
+            first_ = new_element;
+            last_ = new_element;
+        } else {
+            new_element->next = first_;
+            first_ = new_element;
+        }
     }
-    last_->next = new_element;
-    last_ = new_element;
 }
 
-bool SinglyLinkedList::empty() const { return !static_cast<bool>(first_); }
+void SinglyLinkedList::pop_back() { remove(last_); }
 
-// void SinglyLinkedList::remove(int x);
+void SinglyLinkedList::pop_front() { remove(first_); }
+
+auto SinglyLinkedList::find_position(const uint32_t pos) {
+    auto *current{first_};
+    Element *before_current{nullptr};
+    // Find position (for current)
+    for (size_t i = 0; i < pos - 1; ++i) {  // initial current is not null
+        // if end is reached (pos > size)
+        if (!current) {
+            break;
+        }
+        before_current = current;
+        current = current->next;
+    }
+    return std::make_pair(before_current, current);
+}
+
+void SinglyLinkedList::insert(
+    const std::pair<Element *, Element *> &before_current_and_current,
+    const int value) {
+    auto [before_current, current] = before_current_and_current;
+    if (!current) {
+        push_back(value);
+    } else {
+        // position found; add element
+        auto *new_element{new Element{value}};
+        new_element->next = current;
+        if (before_current) {
+            // handle before
+            before_current->next = new_element;
+        }
+    }
+}
+
+void SinglyLinkedList::insert(const uint32_t pos, const int value) {
+    if (!empty()) {
+        // find position
+        const auto before_current_and_current{find_position(pos)};
+        // insert value
+        insert(before_current_and_current, value);
+    } else {
+        // if empty
+        auto *new_element{new Element{value}};
+        first_ = new_element;
+        last_ = new_element;
+    }
+}
+
+bool SinglyLinkedList::empty() const {
+    return !static_cast<bool>(first_) && !static_cast<bool>(last_);
+}
+
+void SinglyLinkedList::remove(Element *element) {
+    if (element) {
+        Element *current{first_};
+        Element *before_current{nullptr};
+        // execute only if current != nullptr and element != nullptr
+        for (; current != nullptr; current = current->next) {
+            if (current == element) {  // found
+                remove(element, before_current);
+                return;
+            }
+            before_current = current;
+        }
+    }
+}
 
 // void SinglyLinkedList::push_front(int x);
 
