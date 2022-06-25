1e068b8deadf9b003c69d4d8e64bf93234e04aab
@damianWu <damian.wolonkiewicz@gmail.com>
2022-06-25 23:20:30 +0200

    Added declarations

--- src/linked_lists/linked_lists/linked_lists.hpp
+++ src/linked_lists/linked_lists/linked_lists.hpp
@@ -3,12 +3,11 @@
 #ifndef SRC_LINKED_LISTS_LINKED_LISTS_LINKED_LISTS_HPP_
 #define SRC_LINKED_LISTS_LINKED_LISTS_LINKED_LISTS_HPP_
 
+#include <initializer_list>
 #include <iostream>
+#include <utility>
 
 namespace singly_linked_list {
-
-// TODO(@damianWu) necessary?
-enum class SEARCH_RESULT { FAIL, SUCCESS };
 
 struct Element {
     int value{};
@@ -18,23 +17,42 @@ struct Element {
 class SinglyLinkedList {
     friend SinglyLinkedList operator+(const SinglyLinkedList &,
                                       const SinglyLinkedList &);
+    friend bool operator==(const SinglyLinkedList &, const SinglyLinkedList &);
+    friend bool operator!=(const SinglyLinkedList &, const SinglyLinkedList &);
 
  public:
+    SinglyLinkedList() = default;
+    SinglyLinkedList(std::initializer_list<int>);
+
     const Element *cfront() const;
     const Element *cback() const;
+    Element *front() const;
+    Element *back() const;
     Element *find(int) const;
     Element *find(Element *) const;
     bool empty() const;
 
-    void remove(int);
+    void remove(Element *e);
     void push_back(int);
+    void push_back(Element *);
     void push_front(int);
-
+    void push_front(Element *);
+    void pop_back();
+    void pop_front();
+    void insert(uint32_t, int);
     ~SinglyLinkedList();
 
  private:
     Element *first_{nullptr};
     Element *last_{nullptr};
+
+ private:
+    void handle_last(Element *, Element *);
+    void handle_first(Element *);
+    void handle_before(Element *, Element *);
+    void remove(Element *, Element *);
+    auto find_position(uint32_t);
+    void insert(const std::pair<Element *, Element *> &, int);
 };
 
 inline SinglyLinkedList::~SinglyLinkedList() {
@@ -46,23 +64,6 @@ inline SinglyLinkedList::~SinglyLinkedList() {
     }
     first_ = nullptr;
     last_ = nullptr;
-}
-
-inline SinglyLinkedList operator+(const SinglyLinkedList &lhs,
-                                  const SinglyLinkedList &rhs) {
-    SinglyLinkedList result;
-
-    auto *lhs_current{lhs.first_};
-    while (lhs_current != nullptr) {
-        result.push_back(lhs_current->value);
-    }
-
-    auto *rhs_current{rhs.first_};
-    while (rhs_current != nullptr) {
-        result.push_back(rhs_current->value);
-    }
-
-    return result;
 }
 
 }  // namespace singly_linked_list
